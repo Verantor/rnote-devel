fb7e5172e383d395cf82b006c9fc72f39c37fa7f
Verantor <verantor@protonmail.com>
2026-08-20 14:04:22 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            ninja
+
+            gcc
+            clang
+            clang-tools
+            python3
+            gnumake
+            cmake
+            meson
+            just
+            git
+
+            appstream
+            gettext
+            desktop-file-utils
+            shared-mime-info
+            appstream-glib
+
+            (rust-bin.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+              ];
+            })
+
+            cargo
+            clippy
+            rustfmt
+          ];
+
+          buildInputs = with pkgs; [
+            glib
+            gtk4
+            libadwaita
+            librsvg
+            alsa-lib
+            libxml2
+          ];
+        };
+      }
+    );
+}
